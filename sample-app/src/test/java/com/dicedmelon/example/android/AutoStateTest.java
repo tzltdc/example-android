515a1e9ebe631c8da591ed5a03c72d17078e9931
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-12-18 01:26:56 -0800

    Test out the minimum configure of ignore file.

--- sample-app/src/test/java/com/dicedmelon/example/android/AutoStateTest.java
+++ sample-app/src/test/java/com/dicedmelon/example/android/AutoStateTest.java
@@ -3,7 +3,6 @@ package com.dicedmelon.example.android;
 import com.google.common.truth.Truth;
 import org.junit.Ignore;
 import org.junit.Test;
-@Ignore("temp_ignore")
 public class AutoStateTest {
 
   @Test public void state() {
